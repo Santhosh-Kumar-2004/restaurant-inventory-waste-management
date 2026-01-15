c9de2c1fbe1c5269cc611cd6b389bd84974435e8
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-15 14:05:50 +0530

    updated the main.py file and added the envs

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(
-    title="Restaurant Inventory & Waste Management",
-    version="0.1.0"
+    title="OTP Auth Verification System(MVP)",
+    description="A simple full-stack auction system MVP using OTP (One-Time Password) instead of passwords.",
+    version="0.7.0",   
 )
 
-@app.get("/")
-def root():
-    return {"message": "Backend is running successfully"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
