8c4a987d0d30767cbebd4227ef257fd94503c969
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-17 00:32:02 +0530

    Adding all the routers endpoint at the meny router file py

--- backend/routers/menu.py
+++ backend/routers/menu.py
@@ -5,3 +5,21 @@ from core import crud
 from schemas.menu import MenuItemCreate, MenuItemResponse
 
 router = APIRouter(prefix="/menu", tags=["Menu"])
+
+@router.post("/", response_model=MenuItemResponse)
+def create_menu(
+    data: MenuItemCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_menu_item(db, data)
+
+@router.get("/", response_model=list[MenuItemResponse])
+def get_menu(db: Session = Depends(get_db)):
+    return crud.get_menu_items(db)
+
+@router.put("/{item_id}/toggle")
+def toggle_menu(item_id: int, db: Session = Depends(get_db)):
+    item = crud.toggle_menu_item(db, item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Menu item not found")
+    return {"message": "Menu availability updated"}
