6a1db450e64f48e25350aeea598b4ae9c21034a7
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 10:30:08 +0530

    Adding the importation

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,2 +1,13 @@
 from schemas.schema import UserCreate, UserLogin, UserResponse, RoleUpdate
 import models
+from core.crud import create_user
+from sqlalchemy.orm import Session
+
+@app.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(
+        db,
+        user.full_name,
+        user.email,
+        user.password
+    )
