bd3a0f3a4918d108665e74972670e39ce83c0fc2
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 10:25:31 +0530

    Updated the file imports

--- backend/core/crud.py
+++ backend/core/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from models import User, UserRole
+from models.models import User, UserRole
 import hashlib
 
 
--- backend/core/database.py
+++ backend/core/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
+# from config import DATABASE_URL
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
