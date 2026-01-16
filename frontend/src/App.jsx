360132f5a138e06e48361257fdb204a3cd6261cb
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 15:26:37 +0530

    Updated the app.jsx and added the react rpouter stuiffs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Inventory from "./pages/Inventory";
+import Orders from "./pages/Orders";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <div>
-      <h1>Restaurant Inventory System</h1>
-      <Login />
-      <hr />
-      <Inventory />
+      <h1>Restaurant System</h1>
+
+      <Routes>
+        <Route path="/" element={<Inventory />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
